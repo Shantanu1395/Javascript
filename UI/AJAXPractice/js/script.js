3f65f53c4130ef23ba73ee7ed7b7d84a7f3163a8
shantanu1395 <shantanu1395@gmail.com>
2018-07-29 19:56:32 +0530

    Added API for NYTimes

--- UI/AJAXPractice/js/script.js
+++ UI/AJAXPractice/js/script.js
@@ -23,6 +23,24 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src= "' + streetviewUrl + '"> ');
 
+    // NYtimes AJAX request 
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+    'api-key': "39accc4fe89046feb86a145b585e7a5a"
+    });
+    $.getJSON(url, function (data){
+       $nytHeaderElem.text("This articles is about "+ cityStr);
+       articles = data.response.docs;
+       console.log(articles); 
+       for (var i = 0; i < articles.length; i++){
+           var article = articles[i];
+           $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>' + '</li>');
+       }
+    }).error(function() {
+        alert( "Handler for .error() called." )
+      });
     return false;
 };
 
