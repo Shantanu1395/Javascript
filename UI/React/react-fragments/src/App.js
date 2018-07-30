2a0f1010744145ad7b1e5833f49e1c4742dd1a7a
shantanu1395 <shantanu1395@gmail.com>
2018-07-30 23:02:36 +0530

    Added react-fragment

---
+++ UI/React/react-fragments/src/App.js
@@ -0,0 +1,31 @@
+//Use case is to remove unwanted outer div tag
+
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+const Temp = () => {
+  return (
+  //<div>   Problem
+  //[       Solution 1
+  //<Fragment> Solution 2
+  <>        
+    <div key="asdf">Hi</div>
+    <div key="fsa">Hello</div>
+  </>  
+  //</Fragment>  
+  //]  
+  //</div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Temp/>
+      </div>
+    );
+  }
+}
+
+export default App;
